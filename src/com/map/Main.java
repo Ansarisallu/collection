2f62341cc28322195a28ca68702c5119ac504160
sallu <salauddinansari07457@gmail.com>
2024-11-17 16:15:48 +0530

    add some extra method

--- src/com/map/Main.java
+++ src/com/map/Main.java
@@ -1,7 +1,33 @@
 package com.map;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        HashMap<Integer, String> hm=new HashMap<>();
+        hm.put(1,"salauddin");
+        hm.put(2,"monu");
+        hm.put(3,"kallu");
+        System.out.println(hm);
+        String s = hm.get(1);
+        System.out.println(s);
+        System.out.println(hm.containsKey(1));
+        System.out.println(hm.containsValue("monu"));
+        Set<Integer> integers = hm.keySet();
+        for (int i:integers){
+            System.out.println("values: "+hm.get(i)+" keys: "+i);
+        }
+        Set<Map.Entry<Integer, String>> entries = hm.entrySet();
+        for(Map.Entry<Integer, String> i:entries){
+            System.out.println(i.getKey()+" : "+i.getValue());
+        }
+        for (Map.Entry<Integer,String> ii:entries){
+            ii.setValue(ii.getValue().toUpperCase());
+        }
+        System.out.println(hm);
+
+
     }
 }
